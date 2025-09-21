20a771d97522503c1e4a7b712b46ecd496c94b21
Akif Ali <akifali9012@gmail.com>
2025-09-21 20:55:31 +0500

    Update elastic_collsion.c

--- elastic_collsion.c
+++ elastic_collsion.c
@@ -1,6 +1,6 @@
 #include "shapes_2D.c"
 //momentum 100% conserved
-function collide_elastically(Sphere sphere1 *s1, Sphere sphere2 *s2, float collision_strength){
+void collide_elastically(Sphere sphere1 *s1, Sphere sphere2 *s2, float collision_strength){
     s1 -> vel = collision_strength/s1->mass;
     s2 -> vel = -collision_strength/s2->mass;
 }
